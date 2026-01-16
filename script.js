45c430430cb8a1ca7045844e18655247de25635c
Gianpsd <gianisikli1@gmail.com>
2026-01-16 21:30:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-/* Für Private, inspo: https://stackoverflow.com/questions/34093732/fade-out-div-on-scroll */
-window.addEventListener('scroll', function() {
-    const imageToFade = document.getElementById('HeaderBild');
 
-    if (imageToFade) {
-        const scrollPosition = window.scrollY;
-        const fadeOutPoint = 900; 
-
-        let newOpacity = 1 - (scrollPosition / fadeOutPoint);
-
-        if (newOpacity < 0) {
-            newOpacity = 0;
-        }
-
-        imageToFade.style.opacity = newOpacity;
-    }
-});
-
-/* Für Private "Individuell" Link Farbe ändern, inspo: https://www.youtube.com/watch?v=b3Sh4oydzoU */
-/* const orangeText = document.querySelector('.intro-text span');
-
-if (orangeText) {
-    orangeText.addEventListener('click', function() {
-        this.classList.toggle('clicked');
-    
-    })
-}*/
 
 
 const toggleCheckbox = document.getElementById('theme-toggle-checkbox');
